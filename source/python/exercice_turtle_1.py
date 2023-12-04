20b06e33d3d73ed1d5510af5e61051fcb02cda44
ychistel <ychistel@ac-caen.fr>
2023-12-04 13:42:01 +0100

    modif exercice 1

--- source/python/exercice_turtle_1.py
+++ source/python/exercice_turtle_1.py
@@ -1,20 +1,5 @@
 from turtle import *
 
-n = 100
-r = 150
-angle = (180-360/n)
-
-circle(r)
-left(90)
-for i in range(n):
-    forward(r)
-    right(angle)
-    forward(r)
-    left(180)
-hideturtle()
-
-
-
 def angle(n):
     return 180-360/n
 
@@ -27,6 +12,4 @@ def roue(r,n):
         right(a)
         forward(r)
         left(180)
-        
-roue(100,20)
-hideturtle()
+
